b4342d0d127aeb7a047e36c510d9a4a284774adb
Dmitry Rozhkov <dmitry.rozhkov@linux.intel.com>
2018-08-17 11:09:52 +0300

    specify exactly the modes where auto-complete works

--- init.el
+++ init.el
@@ -160,6 +160,59 @@ vi style of % jumping to matching brace."
 
 ;; autocomplete
 (require 'auto-complete-config)
+(setq ac-modes '(emacs-lisp-mode
+                 lisp-mode
+                 lisp-interaction-mode
+                 slime-repl-mode
+                 nim-mode
+                 c-mode
+                 cc-mode
+                 c++-mode
+                 objc-mode
+                 swift-mode
+                 java-mode
+                 malabar-mode
+                 clojure-mode
+                 clojurescript-mode
+                 scala-mode
+                 scheme-mode
+                 ocaml-mode
+                 tuareg-mode
+                 coq-mode
+                 haskell-mode
+                 agda-mode
+                 agda2-mode
+                 perl-mode
+                 cperl-mode
+                 python-mode
+                 ruby-mode
+                 lua-mode
+                 tcl-mode
+                 ecmascript-mode
+                 javascript-mode
+                 js-mode
+                 js-jsx-mode
+                 js2-mode
+                 js2-jsx-mode
+                 coffee-mode
+                 php-mode
+                 css-mode
+                 scss-mode
+                 less-css-mode
+                 elixir-mode
+                 makefile-mode
+                 sh-mode
+                 fortran-mode
+                 f90-mode
+                 ada-mode
+                 xml-mode
+                 sgml-mode
+                 web-mode
+                 ts-mode
+                 sclang-mode
+                 verilog-mode
+                 qml-mode
+                 apples-mode))
 (ac-config-default)
 
 (global-set-key (kbd "C-x g") 'magit-status)

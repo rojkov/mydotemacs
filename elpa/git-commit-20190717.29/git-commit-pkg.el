f3b07a57294aca04e10d2e734a7596b813ff66ea
Dmitry Rozhkov <dmitry.rozhkov@linux.intel.com>
2019-08-26 16:41:32 +0300

    upgrade installed packages

---
+++ elpa/git-commit-20190717.29/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20190717.29" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "5a2786cc005c1dfb698b6c2fc1dd592c344b0e4a" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")

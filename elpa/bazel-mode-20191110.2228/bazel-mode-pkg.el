605377e53634da440929d0536b7c9809eb4f090e
Dmitry Rozhkov <dmitry.rojkov@gmail.com>
2020-03-21 21:23:08 +0200

    upgrade installed packages 21.03.2020

---
+++ elpa/bazel-mode-20191110.2228/bazel-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "bazel-mode" "20191110.2228" "A major mode for editing Bazel files" '((emacs "26")) :commit "7798e33cba5db14eb2c06ddcbc4e8fa361eb679c" :keywords '("languages" "bazel") :authors '(("Neri Marschik")) :maintainer '("Neri Marschik") :url "https://github.com/codesuki/bazel-mode")

54248df169282c374f49825a0c8522a3533c2eb5
Dmitry Rozhkov <dmitry.rojkov@gmail.com>
2020-04-30 12:24:44 +0300

    upgrade ELPA packages

---
+++ elpa/bazel-mode-20200424.1953/bazel-mode-pkg.el
@@ -0,0 +1,8 @@
+(define-package "bazel-mode" "20200424.1953" "Emacs major mode for editing Bazel BUILD and WORKSPACE files"
+  '((emacs "26.1"))
+  :keywords
+  '("build tools" "languages")
+  :url "https://github.com/bazelbuild/emacs-bazel-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- elpa/system-packages-20200310.1510/system-packages-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "system-packages" "20200310.1510" "functions to manage system packages" '((emacs "24.3")) :commit "449dcdf4fe22874c9d91ee8d929ebb8a41b1bac6" :authors '(("J. Alexander Branham" . "alex.branham@gmail.com")) :maintainer '("J. Alexander Branham" . "alex.branham@gmail.com") :url "https://gitlab.com/jabranham/system-packages")
